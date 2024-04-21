48b480806d8544409e1e6ab677078042ef927c39
StachRach <srachwal2000@gmail.com>
2024-04-22 00:10:10 +0200

    changed color palette, today marked by different color

--- CalendarScreen.py
+++ CalendarScreen.py
@@ -11,7 +11,7 @@ import calendar
 class CalendarScreen(Screen):
     def __init__(self, **kwargs):
         super(CalendarScreen, self).__init__(**kwargs)
-        self.layout = BoxLayout(orientation='vertical', spacing=5)
+        self.layout = BoxLayout(orientation='vertical', spacing=5, padding=10)
         self.add_widget(self.layout)
 
         self.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
@@ -33,23 +33,28 @@ class CalendarScreen(Screen):
         self.info.add_widget(Label(text='Calendar Screen', size_hint=(1, .2)))
         self.info.add_widget(self.month_view)
 
-        self.menu.add_widget(Button(text='<', on_press=self.change_month,
+        self.menu.add_widget(Button(text='<', on_press=self.change_month, background_color=(.2, .5, .3, 1),
                                     size_hint=(.15, .3), pos_hint={'center_x': .5, 'center_y': .5}))
         self.menu.add_widget(self.info)
-        self.menu.add_widget(Button(text='>', on_press=self.change_month,
+        self.menu.add_widget(Button(text='>', on_press=self.change_month, background_color=(.2, .5, .3, 1),
                                     size_hint=(.15, .3), pos_hint={'center_x': .5, 'center_y': .5}))
 
-        self.calendar = Calendar(self.calendar)
-        self.button = Button(text='Back', on_press=self.back, size_hint=(1, .2))
+        self.calendar = Calendar(self.calendar, self.date)
+
+        self.btnBox = BoxLayout(orientation='horizontal', size_hint=(.7, .2), pos_hint={'center_x': .5, 'center_y': .5})
+        self.button = Button(text='Back', on_press=self.back, background_color=(.21, .35, .47, 1),
+                             size_hint=(.5, .7), pos_hint={'center_x': .5, 'center_y': .5})
+
+        self.btnBox.add_widget(self.button)
 
         self.layout.add_widget(self.menu)
         self.layout.add_widget(self.calendar)
-        self.layout.add_widget(self.button)
+        self.layout.add_widget(self.btnBox)
 
     def change_month(self, symbol):
         self.info.remove_widget(self.month_view)
         self.layout.remove_widget(self.calendar)
-        self.layout.remove_widget(self.button)
+        self.layout.remove_widget(self.btnBox)
 
         if symbol.text == '<' and self.date['month'] > 1:
             self.date['month'] -= 1
@@ -66,11 +71,11 @@ class CalendarScreen(Screen):
         self.month_view = Label(text=self.active_info, size_hint=(1, .2))
 
         self.calendar = calendar.monthcalendar(self.date['year'], self.date['month'])
-        self.calendar = Calendar(self.calendar)
+        self.calendar = Calendar(self.calendar, self.date)
 
         self.info.add_widget(self.month_view)
         self.layout.add_widget(self.calendar)
-        self.layout.add_widget(self.button)
+        self.layout.add_widget(self.btnBox)
 
     def get_dates(self):
         pass
@@ -80,20 +85,32 @@ class CalendarScreen(Screen):
 
 
 class Calendar(Screen):
-    def __init__(self, date, **kwargs):
+    def __init__(self, days, date, **kwargs):
         super(Calendar, self).__init__(**kwargs)
         self.layout = GridLayout(cols=7, spacing=5)
         self.add_widget(self.layout)
 
+        self.calendar = days
         self.date = date
 
         days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         for day in days:
             self.layout.add_widget(Label(text=day))
 
-        for week in self.date:
-            for day in week:
-                if day == 0:
-                    self.layout.add_widget(Label(text=''))
+        for week in self.calendar:
+            for i, day in enumerate(week):
+                btn = Button()
+                if i == 5 or i == 6:
+                    btn.background_color = (1, 0, 0, 1)
                 else:
-                    self.layout.add_widget(Button(text=str(day)))
+                    btn.background_color = (.21, .35, .47)
+
+                if (day == self.date['day'] and self.date['month'] == datetime.now().month
+                        and self.date['year'] == datetime.now().year):
+                    btn.background_color = (.2, .8, .2, 1)
+
+                if day == 0:
+                    btn.text = ''
+                else:
+                    btn.text = str(day)
+                self.layout.add_widget(btn)
